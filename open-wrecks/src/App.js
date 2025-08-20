cec931d835925e798a04e1dd9d5e34a9e71d07ae
Alfredredbird <105014217+Alfredredbird@users.noreply.github.com>
2025-08-20 10:07:07 -0700

    added new maps

--- open-wrecks/src/App.js
+++ open-wrecks/src/App.js
@@ -73,12 +73,17 @@ function App() {
   const [measureMode, setMeasureMode] = useState(false);
   const [selectedMarkers, setSelectedMarkers] = useState([]);
   const [mapStyle, setMapStyle] = useState("osm"); // default
-  const mapStyles = {
+ const mapStyles = {
   osm: {
     name: "OpenStreetMap",
     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   },
+  osmHot: {
+  name: "OpenStreetMap HOT",
+  url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
+},
   cartoLight: {
     name: "Carto Light",
     url: "https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png",
@@ -88,6 +93,21 @@ function App() {
     name: "Carto Dark",
     url: "https://cartodb-basemaps-a.global.ssl.fastly.net/dark_all/{z}/{x}/{y}{r}.png",
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>'
+  },
+  stamenToner: {
+    name: "Stamen Toner",
+    url: "https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png",
+    attribution: '© <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> © <a href="https://stamen.com/" target="_blank">Stamen Design</a> © <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> © <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+  },
+  stamenWatercolor: {
+    name: "Stamen Watercolor",
+    url: "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg",
+    attribution: '© <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> © <a href="https://stamen.com/" target="_blank">Stamen Design</a> © <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+  },
+  stamenTerrain: {
+    name: "Stamen Terrain",
+    url: "https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png",
+    attribution: '© <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> © <a href="https://stamen.com/" target="_blank">Stamen Design</a> © <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> © <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
   }
 };
 
